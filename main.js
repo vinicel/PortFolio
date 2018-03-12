4c680b541b8fd5b3fe883ce307d465d982c97419
VicoRch <victor.roche.graph@gmail.com>
2018-03-12 10:55:12 +0100

    up

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 
   var Loader = document.querySelector(".Loader");
   var box1 = document.getElementById('box1');
-  var spacerRLDP = document.getElementById('Spacer1')
+  var box2 = document.getElementById('box2');
+  var box3 = document.getElementById('box3');
 
+  var spacerRLDP = document.getElementById('Spacer1')
+  var spacerPort = document.getElementById('Spacer2')
+  var spacerRBF = document.getElementById('Spacer3')
 
   setTimeout(function() {
     Loader.classList.add("Loader-animation");
@@ -20,3 +24,25 @@
     spacerRLDP.style.width = "0px";
     spacerRLDP.style.transition = '0.2s';
   });
+
+box2.addEventListener("mouseenter", function() {
+    spacerPort.style.width = "40px";
+    spacerPort.style.transition = '0.2s';
+  });
+
+  box2.addEventListener("mouseleave", function() {
+
+    spacerPort.style.width = "0px";
+    spacerPort.style.transition = '0.2s';
+  });
+
+  box3.addEventListener("mouseenter", function() {
+      spacerRBF.style.width = "40px";
+      spacerRBF.style.transition = '0.2s';
+    });
+
+    box3.addEventListener("mouseleave", function() {
+
+      spacerRBF.style.width = "0px";
+      spacerRBF.style.transition = '0.2s';
+    });
