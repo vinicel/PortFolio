79965c6ea9cb508ead18d9b68a258b4eb08d36f1
VicoRch <victor.roche.graph@gmail.com>
2018-03-12 11:00:44 +0100

    finish Home

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 
   var Loader = document.querySelector(".Loader");
+
   var box1 = document.getElementById('box1');
   var box2 = document.getElementById('box2');
   var box3 = document.getElementById('box3');
+  var boxAbout = document.getElementById('boxAbout')
 
   var spacerRLDP = document.getElementById('Spacer1')
   var spacerPort = document.getElementById('Spacer2')
   var spacerRBF = document.getElementById('Spacer3')
-
+  var spacerAbout = document.getElementById('spacerAbout')
   setTimeout(function() {
     Loader.classList.add("Loader-animation");
     Loader.style.zIndex = "-1";
@@ -46,3 +48,14 @@ box2.addEventListener("mouseenter", function() {
       spacerRBF.style.width = "0px";
       spacerRBF.style.transition = '0.2s';
     });
+
+    boxAbout.addEventListener("mouseenter", function() {
+        spacerAbout.style.width = "33px";
+        spacerAbout.style.transition = '0.2s';
+      });
+
+      boxAbout.addEventListener("mouseleave", function() {
+
+        spacerAbout.style.width = "0px";
+        spacerAbout.style.transition = '0.2s';
+      });
