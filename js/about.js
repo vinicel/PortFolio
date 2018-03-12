59b4a00804e45cf774378437e7a1942d9a1cfa7b
VicoRch <victor.roche.graph@gmail.com>
2018-03-12 14:47:43 +0100

    look js

---
+++ js/about.js
@@ -0,0 +1,31 @@
+var Loader = document.querySelector(".Loader");
+
+
+var boxAbout = document.getElementById('boxAbout')
+var spacerAbout = document.getElementById('spacerAbout')
+var cv = document.getElementById('cv')
+
+
+var boxAbout = document.getElementById('boxAbout')
+var spacerAbout = document.getElementById('spacerAbout')
+setTimeout(function() {
+  Loader.classList.add("Loader-animation");
+  Loader.style.zIndex = "-1";
+
+}, 700);
+
+cv.addEventListener("mouseenter", function() {
+    cv.style.padding = " O O 2Opx O";
+    cv.style.transition = '0.2s';
+  });
+
+boxAbout.addEventListener("mouseenter", function() {
+    spacerAbout.style.width = "33px";
+    spacerAbout.style.transition = '0.2s';
+  });
+
+  boxAbout.addEventListener("mouseleave", function() {
+
+    spacerAbout.style.width = "0px";
+    spacerAbout.style.transition = '0.2s';
+  });
